ee7af7e2f1ad8199cb93ca44ffc7c1c55291d72c
Kostiantyn Siharov <ksigaryov@gmail.com>
2023-06-09 08:34:45 +0200

    Added a slash and a question mark to the end of the URL

--- main.rb
+++ main.rb
@@ -2,18 +2,12 @@ require 'sinatra'
 require 'sinatra/json'
 require 'sinatra/reloader' if development?
 
-get '/' do
+get '/?' do
   @message = 'Main page!'
   erb :home
 end
 
-get '/about' do
+get '/about/?' do
   @title = "About"
   erb :about
-end
-
-get '/:name' do
-  name = params[:name]
-  something = { information: name }
-  json something
 end
