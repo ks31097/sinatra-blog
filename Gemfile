95c03ad7e6625a08ae55f7cca4ba70d99875b587
Kostiantyn Siharov <ksigaryov@gmail.com>
2023-06-18 17:24:23 +0200

    Added sessions

--- Gemfile
+++ Gemfile
@@ -7,5 +7,9 @@ ruby File.read(".ruby-version").strip
 gem "sinatra", "~> 3.0"
 gem "sinatra-contrib", "~> 3.0"
 gem "puma", "~> 6.2"
-gem "rack-test", "~> 2.1"
-gem "rspec", "~> 3.12"
+gem "sysrandom", "~> 1.0"
+
+group :test do
+  gem "rack-test", "~> 2.1"
+  gem "rspec", "~> 3.12"
+end
--- main.rb
+++ main.rb
@@ -1,16 +1,33 @@
 require 'sinatra'
 require 'sinatra/json'
 require 'sinatra/reloader' if development?
+require 'securerandom'
+
+# set :views, './app/views'
+
+set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
+
+configure do
+  enable :sessions
+end
 
 get '/?' do
+  session[:info] = 'turn on'
   @title = 'Hello, world!'
   @message = 'Main page!'
+  @home_info = session[:info]
   erb 'home'.to_sym
 end
 
 get '/about/?' do
   @title = "About"
+  @about_info = session[:info]
   erb 'about'.to_sym
+end
+
+get '/logout/?' do
+  session.clear
+  redirect '/about/?'
 end
 
 not_found do
