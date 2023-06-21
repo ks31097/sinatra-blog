b8741f486d43f9202c1210e7da70f28b921d3bf0
Kostiantyn Siharov <ksigaryov@gmail.com>
2023-06-21 21:59:54 +0200

    Created modular Sinatra application

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,22 @@
+require './app/controllers/app_controller'
+
+class UserController < AppController
+  get '/?' do
+    session[:info] = 'turn on'
+    @title = 'Hello, world!'
+    @message = 'Main page!'
+    @home_info = session[:info]
+    erb 'home'.to_sym
+  end
+
+  get '/about/?' do
+    @title = "About"
+    @about_info = session[:info]
+    erb 'about'.to_sym
+  end
+
+  get '/logout/?' do
+    session.clear
+    redirect '/about/?'
+  end
+end
