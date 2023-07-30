ed036ed3ea0cf84267cfd482e486b5b62441efc8
Kostiantyn Siharov <ksigaryov@gmail.com>
2023-07-30 13:08:04 +0200

    Added database

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,5 @@
 require './app/controllers/app_controller'
+require './app/models/article'
 
 class UserController < AppController
   get '/?' do
@@ -6,11 +7,13 @@ class UserController < AppController
     title 'Hello, world!'
     @message = 'Main page!'
     @home_info = session[:info]
+    @articles = Article.all
     erb 'index'.to_sym
+    # Article.find_or_create_by(title: 'First article').to_json
   end
 
   get '/about/?' do
-    title "About"
+    title 'About'
     @about_info = session[:info]
     erb 'about'.to_sym
   end
@@ -18,5 +21,10 @@ class UserController < AppController
   get '/logout/?' do
     session.clear
     redirect '/about/?'
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb 'show'.to_sym
   end
 end
---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+require "sinatra/activerecord"
+
+class Article < ActiveRecord::Base
+ # validates_presence_of :title
+end
