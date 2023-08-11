b9bc5a1997b12f103a66bf47af7ecd5c35c19649
Kostiantyn Siharov <ksigaryov@gmail.com>
2023-08-11 17:12:42 +0200

    Added into DB information about the author

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_01_121657) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.text "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
