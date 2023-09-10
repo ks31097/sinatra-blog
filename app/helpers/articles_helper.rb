81d9ccbbbd26943d0dbe2fc1f25b88810d08929e
Kostiantyn Siharov <ksigaryov@gmail.com>
2023-09-10 17:41:21 +0200

    Removed JSON methods

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,13 +1,9 @@
+# frozen_string_literal: true
+
 module ArticlesHelper
-
   # @title for the views
-  def title(title='Blog')
+  def title(title = 'Blog')
     @title = title
-  end
-
-  # @show the current time
-  def time_now
-    Time.now
   end
 
   # @find all articles
@@ -15,32 +11,16 @@ module ArticlesHelper
     Article.all
   end
 
+  private
+
   # @find the article
   def find_article
     Article.find(article_id)
   end
 
-  # @format the article sinatra-flash errors
-  def article_error(article)
-    article.errors.full_messages
-  end
-
   # @create the article
   def create_article
     Article.new(article_params)
-  end
-
-  private
-
-  # @format body data_json
-  def data_json(created: false)
-    payload = JSON.parse request.body.read
-      if created
-        payload['created_at'] = time_now
-        payload['updated_at'] = time_now
-      end
-
-    payload
   end
 
   # @update_article
@@ -64,20 +44,11 @@ module ArticlesHelper
 
   # @error: user not logged in
   def redirect_if_not_logged_in
-    flash.next[:error_log_in] = 'You are not log in!'
+    flash.next[:warning] = 'You are not log in!'
     redirect '/log_in'
   end
 
   def article_author(user_id)
-    (User.find(user_id)).full_name
+    User.find(user_id).full_name
   end
-
-  def json_user_logged_in?
-    session[:json_user_id]
-  end
-
-  def json_not_logged_message
-    'You are not log in!'
-  end
-
 end
