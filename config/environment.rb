a53966e32b8dc45286ab4a812a681563d6a82e90
Kostiantyn Siharov <ksigaryov@gmail.com>
2023-09-10 17:18:33 +0200

    Added rubocop frozen string

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 require 'bundler' # Include all the gem listed in Gemfile
 Bundler.require
 
 require_relative '../app/controllers/about_controller'
 require_relative '../app/controllers/user_controller'
 require_relative '../app/controllers/article_controller'
-require_relative File.join(File.dirname(__FILE__), '../app/controllers/application_controller.rb') # __FILE__ is the current file
+# __FILE__ is the current file
+require_relative File.join(File.dirname(__FILE__), '../app/controllers/application_controller.rb')
