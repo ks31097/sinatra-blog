1def3559cae5eae7992356b70c275e8f67b04051
Kostiantyn Siharov <ksigaryov@gmail.com>
2023-09-10 17:45:27 +0200

    Added rubocop frozen string, removed bcrypt code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,13 @@
-require "sinatra/activerecord"
-require 'bcrypt'
+# frozen_string_literal: true
+
+require 'sinatra/activerecord'
 
 class User < ActiveRecord::Base
-
   has_secure_password
 
   has_many :articles
 
-  # users.password_hash in the database is a :string
-  # # include BCrypt
-
-  # def password
-    # @password ||= Password.new(password_hash)
-  # end
-
-  # def password=(new_password)
-    # @password = Password.create(new_password)
-    # self.password_hash = @password
-  # end
-
   validates :full_name, presence: true, length: { minimum: 4 }
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true, length: { minimum: 5 }
 end
